e6dc3963958d00b2f7d117e6c2c4d82ed114ed75
PythonHippie <42512235+PythonHippie@users.noreply.github.com>
2019-06-04 12:54:57 +0530

    all test classes are released

--- src/test/java/com/javatechie/StringPalindrome/AppTest.java
+++ src/test/java/com/javatechie/StringPalindrome/AppTest.java
@@ -13,16 +13,17 @@ public class AppTest {
 	App app = new App();
 	boolean expected = true;
 
-	
+	@Test
 	public void isPlaindromeTest() {
 		assertEquals(expected, app.isPalindrome(input1));
 	}
 
-	
+	@Test
 	public void isNotPlaindromeTest() {
 		assertEquals(false, app.isPalindrome("abc"));
 	}
 	
+	@Test(expected = IllegalArgumentException.class)
 	public void isNotPlaindromeExceptionTest() {
 		assertEquals(false, app.isPalindrome(null));
 	}
